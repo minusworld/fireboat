ba224217c9d8b5b9469fd6f696fde4c64a66ad85
SMK <sharikurland@gmail.com>
2015-11-11 17:20:34 -0500

    Adding the script for autorotating the servo

--- autoRotate.sh
+++ autoRotate.sh
@@ -6,5 +6,14 @@ END=220
 
 for (( c=$START; c<=$END; c++ ))
 do
-   echo $c > $SERV
+	echo 0=$c > $SERV
+	echo 1=$c > $SERV
+done
+
+# Aaaand back
+
+for (( i=$END; i <=$START; i-- ))
+do
+        echo 0=$c > $SERV
+        echo 1=$c > $SERV
 done
