8056dd457747a10f2945d6b37b848f04a15bd32a
minusworld <locrian.vii@gmail.com>
2015-12-08 19:30:10 -0500

    Added launcher/main loop, fireboat_go.py.  Made fire_scan stepwise instead of continuous loop

--- scripts/fire_scan.py
+++ scripts/fire_scan.py
@@ -2,90 +2,78 @@ import time
 import targeting
 import os
 
-def servo_out(servoNum, servoValue):
-	with open('/dev/servoblaster', 'a') as f:
-		f.write('%d=%d\n' % (servoNum, servoValue))
 
-def main():
-	import time
+class ServoControl:
 
-	# servo0 = pan
-	# servo1 = tilt
-	servo0_max = 150
-	servo0_min = 80
-	servo1_max = 150
-	servo1_min = 100
+    def __init__(self):
+        self.servo0_max = 150
+        self.servo0_min = 80
+        self.servo1_max = 150
+        self.servo1_min = 100
 
-	servo0_position = servo0_min
-	servo1_position = servo1_min
+        self.servo0_position = servo0_min
+        self.servo1_position = servo1_min
 
-	motion = 'pan' # values of pan or tilt
+        self.motion = 'pan' # values of pan or tilt
 
-	servo0_direction = 'up' # values of up or down
-	servo1_direction = 'up' # values of up or down
+        self.servo0_direction = 'up' # values of up or down
+        self.servo1_direction = 'up' # values of up or down
 
-	fire_inView = False
-	fire_targeted  = False
+	    self.fire_inView = False
+	    self.fire_targeted  = False
 
-	servo_out(0, servo0_position)
-	time.sleep(1)
-	servo_out(1, servo1_position)
+        self.servo_out(0, self.servo0_position)
+        time.sleep(1)
+        self.servo_out(1, self.servo1_position)
 
-	while not fire_inView:
-		if motion == 'pan' and servo0_direction == 'up':
-			servo0_position += 10
-			servo_out(0, servo0_position)
-		elif motion == 'pan' and servo0_direction == 'down':
-			servo0_position -= 10
-			servo_out(0, servo0_position)
-		elif motion == 'tilt' and servo1_direction == 'up':
-			servo1_position += 10
-			motion = 'pan'
-			servo_out(1, servo1_position)
-		elif motion == 'tilt' and servo1_direction == 'down':
-			servo1_position -= 10
-			motion = 'pan'
-			servo_out(1, servo1_position)
+    def servo_out(self, servoNum, servoValue):
+        with open('/dev/servoblaster', 'a') as f:
+            f.write('%d=%d\n' % (servoNum, servoValue))
 
-		time.sleep(1)
+    def step(self):
+        if not fire_inView:
+            if motion == 'pan' and servo0_direction == 'up':
+                servo0_position += 10
+                servo_out(0, servo0_position)
+            elif motion == 'pan' and servo0_direction == 'down':
+                servo0_position -= 10
+                servo_out(0, servo0_position)
+            elif motion == 'tilt' and servo1_direction == 'up':
+                servo1_position += 10
+                motion = 'pan'
+                servo_out(1, servo1_position)
+            elif motion == 'tilt' and servo1_direction == 'down':
+                servo1_position -= 10
+                motion = 'pan'
+                servo_out(1, servo1_position)
 
-        score = (-1, -1)    # needed for scoping
-        try:
-            score = targeting.target_from_file("test0.png")
-        except FileNotFoundError as e:
-            continue
-            
-		if servo0_position > servo0_max:
-			servo0_direction = 'down'
-			servo0_position -= 10
-			servo_out(0, servo0_position)
-			motion = 'tilt'
-		elif servo0_position < servo0_min:
-			servo0_direction = 'up'
-			servo0_position += 10
-			servo_out(0, servo0_position)
-			motion = 'tilt'
-		elif servo1_position > servo1_max:
-			servo1_direction = 'down'
-			servo1_position -= 10
-			servo_out(1, servo1_position)
-			motion = 'pan'
-		elif servo1_position < servo1_min:
-			servo1_direction = 'up'
-			servo1_position += 10
-			servo_out(1, servo1_position)
-			motion = 'pan'
+            time.sleep(1)
 
-		time.sleep(1)
+            score = (-1, -1)    # needed for scoping. score format: (grid_index, grid_score)
+            try:
+                score = targeting.target_from_file("test0.png")
+            except FileNotFoundError as e:
+                return  
+                
+            if servo0_position > servo0_max:
+                servo0_direction = 'down'
+                servo0_position -= 10
+                servo_out(0, servo0_position)
+                motion = 'tilt'
+            elif servo0_position < servo0_min:
+                servo0_direction = 'up'
+                servo0_position += 10
+                servo_out(0, servo0_position)
+                motion = 'tilt'
+            elif servo1_position > servo1_max:
+                servo1_direction = 'down'
+                servo1_position -= 10
+                servo_out(1, servo1_position)
+                motion = 'pan'
+            elif servo1_position < servo1_min:
+                servo1_direction = 'up'
+                servo1_position += 10
+                servo_out(1, servo1_position)
+                motion = 'pan'
 
-	#search for fire somehow
-	#if (somehow see fire)
-	#	fire_inView = True
-
-
-#while not fire_targeted:
-	#do fine tuning of pan-tilt to center fire
-
-
-if __name__ == "__main__":
-    main()
+            time.sleep(1)
---
+++ scripts/fireboat_go.py
@@ -0,0 +1,15 @@
+import os
+from fire_scan import ServoControl
+
+def pump_on():
+    os.system("gpio write 7 {0}".format(1))
+
+def pump_off():
+    os.system("gpio write 7 {0}".format(0))
+
+def go():
+    servo_control = ServoControl()
+    while 1:
+        os.sytstem("../bin/raspberrypi_video")
+        servo_control.step()
+    
